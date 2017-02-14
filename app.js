e4f98b300b6cd55f5596e56b960ecd45c5e6738b
Oniohm <jnelson6@ggc.edu>
2017-02-13 19:02:24 -0500

    ui test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/public', express.static(path.join(__dirname+'/public')));
 
 app.get('/', function(req,res){
   res.render('Townsquare', {
-
+  
   });
 });
 
--- public/scripts/moblie.js
+++ public/scripts/moblie.js
@@ -1,13 +1,16 @@
 var paine = document.getElementById('paine');
-
+var beginning = document.getElementById('bottomNavLastPage');
 // Listeners
 window.addEventListener('scroll',scrollFadeOut,false);
 paine.addEventListener('click', scrollFadeIn,false);
+beginning.addEventListener('click',windowchange,false);
 
 
 
-
-
+// Listener functions
+function windowchange(){
+  window.location = '/index';
+}
 function scrollFadeIn(){
   document.getElementById('bottomNavHolder').style.opacity = "1.0";
 }
