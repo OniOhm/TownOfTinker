50f9ba77fb9c07d958f5942206dd03c36f16c6c1
jamez <jnelson6@ggc.edu>
2017-02-23 23:00:52 -0500

    database basics added,chapter entry added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const handlebars = require('express-handlebars')
 .create({defaultLayout: 'main'});
 const mongoose = require('mongoose');
 var Chap = require('./public/db/Chap.model.js');
+var bodyParser = require('body-parser');
 mongoose.promise = Promise;
 mongoose.connect('mongodb://localhost/bookKeeper');
 
@@ -13,9 +14,18 @@ app.engine('handlebars', handlebars.engine);
 
 app.set('port', process.env.PORT || 3000);
 app.use('/public', express.static(path.join(__dirname+'/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
 
 app.get('/', function(req,res){
   res.render('Townsquare', {
+
+  });
+});
+app.get('/newChapter',function(req,res){
+  res.render('addNewChapter',{
 
   });
 });
@@ -29,6 +39,44 @@ app.get('/Archive', function(req,res){
     }
   });
 });
+app.get('/Chapters/:id', function(req,res){
+  console.log('geting one Chapter');
+  Chap.findOne({
+    _id: req.params.id
+  })
+  .exec(function(err,chapters){
+    if(err){
+      res.send('error has occured');
+    }else{
+      res.json(chapters);
+    }
+  });
+});
+
+app.post('/addNewChapter',function(req,res){
+  var newChapter = new Chap();
+  newChapter.title = req.body.title;
+  newChapter.page = req.body.page;
+
+  newChapter.save(function(err,chapters){
+    if(err){
+      res.send('error saving book');
+    }else{
+      console.log(chapters);
+      res.send(chapters);
+    }
+  });
+});
+// app.post('/newChap2',function(req,res){
+//   Chap.create(req.body,function(err,book){
+//     if(err){
+//       res.send('error creating Chap');
+//     }else{
+//       console.log(Chap);
+//       res.send(Chap);
+//     }
+//   })
+// })
 
 app.listen(app.get('port'), function(){
   console.log("Hey listen! Port "+ app.get('port')+ " is listening in")
